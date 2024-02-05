938064186e36ee6078a9143ab99e8ad2a03e0edb
nelson nwajie <nelson@busyqa.com>
2024-02-05 03:59:07 +0000

    new commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Welcome, busyqa! This is a simple Flask app.'
+    return '<h1><b>Welcome, busyqa! This is a simple Flask app. By Nelson<?b></h1>'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0',port=8000)
